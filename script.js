361afbfb959627053bf1ec120a7d21d4e9e80fd4
Djotajo <20823564+Djotajo@users.noreply.github.com>
2023-08-22 18:14:11 +0200

    Create gameboard module with fill function

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-const gameboard = ()();
+const board = document.querySelector("#gameboard");
+
+const gameboard = (() => {
+  const state = ["x", "o", "x", "o", "x", "o", "x", "o", "x"];
+  const fill = () => {
+    state.forEach((element) => {
+      const div = document.createElement("div");
+      div.classList.add("field");
+      div.innerHTML = element;
+      board.appendChild(div);
+    });
+  };
+  return {
+    fill,
+  };
+})();
+
+gameboard.fill();
+
+// bookInfo.splice(0, 3).forEach((info) => {
+//     const para = document.createElement("p");
+//     para.innerHTML = info;
+//     div.appendChild(para);
+//   });
+//   const readBook = readButton(book);
+//   div.appendChild(readBook);
+//       });
